182f5880e9bd740ab542dac1c01cd0bd5f411c37
Light Focus <952646764@qq.com>
2019-03-26 22:24:35 +0800

    first version

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <title>搜索</title>
   </head>
   <body>
-     <div class="container marketing">
+  <div class="container marketing">
     <div class="container">
         <div class="py-5 text-center">
         <img class="d-block mx-auto mb-4"src="https://lightfocus-1256547063.cos.ap-hongkong.myqcloud.com/website.png" width="300">
@@ -22,10 +22,16 @@
           <label for="inputEmail" class="sr-only">请输入您要查找的内容</label>
           <input type="text" name="search" id="input" class="form-control" placeholder="" required autofocus>
           <br>
-          <button class="btn btn-lg btn-primary btn-block" type="submit" name="web" value="google">搜索谷歌</button>
+          <div style="margin:0 auto; max-width:300px;">
+            <button class="btn btn-lg btn-primary btn-block" type="submit" name="web" value="google">搜索谷歌</button>
+          </div>
         </form>
       </div>
    </div>
+   <footer class="my-5 pt-5 text-muted text-center text-small">
+    <p class="mb-1">&copy; 2019 Light Focus</p>
+    <p class="mb-1">All Rights Reserved.</p>
+   </footer>
  </div>
  <script src="bootstrap.min.js"></script>
   </body>
